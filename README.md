7c94179f90de2f87476ad3a919225984d0010f28
mattds825 <matthewgdasilva25@gmail.com>
2020-06-25 11:36:06 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# iframe_side_ads
+
+This is from from the tutorail of how to add side ads to a flutter web project
---
+++ android/app/src/main/kotlin/com/example/iframe_side_ads/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.iframe_side_ads
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
